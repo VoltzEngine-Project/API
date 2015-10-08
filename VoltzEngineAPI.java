8380690e34ae5c290d283c4bf2f2e60c2bfd587c
Robert Seifert <rseifert.phone@gmail.com>
2015-10-07 22:36:27 -0400

    Version #0.2.0, Changed return type of IExternalInventory#getContainedItems() to Collection<ItemStack> from ItemStack[]

--- VoltzEngineAPI.java
+++ VoltzEngineAPI.java
@@ -5,5 +5,5 @@ package com.builtbroken.mc.api;
  */
 public class VoltzEngineAPI
 {
-    public static final String VERSION = "0.1.1";
+    public static final String VERSION = "0.2.0";
 }
--- tile/node/IExternalInventory.java
+++ tile/node/IExternalInventory.java
@@ -3,6 +3,8 @@ package com.builtbroken.mc.api.tile.node;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 
+import java.util.Collection;
+
 /**
  * @author DarkGuardsman
  */
@@ -11,7 +13,7 @@ public interface IExternalInventory extends IInventory
 	/**
 	 * Gets the inventory array. ForgeDirection.UNKOWN must return all sides
 	 */
-	ItemStack[] getContainedItems();
+	Collection<ItemStack> getContainedItems();
 
 	/**
 	 * Deletes all the items in the inventory
