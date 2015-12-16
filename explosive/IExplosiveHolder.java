2229b251d7591374646b5e1ef2611736b8827651
Robert Seifert <rseifert.phone@gmail.com>
2015-12-16 15:04:03 -0500

    Added additional data getters to IExplosiveHolder

--- explosive/IExplosiveHolder.java
+++ explosive/IExplosiveHolder.java
@@ -2,17 +2,34 @@ package com.builtbroken.mc.api.explosive;
 
 import net.minecraft.nbt.NBTTagCompound;
 
-/** Anything that can contain an explosive in a way that the explosive can be inserted/set
+/**
+ * Anything that can contain an explosive in a way that the explosive can be inserted/set
  * Created by robert on 2/1/2015.
  */
 public interface IExplosiveHolder extends IExplosive
 {
     /**
      * Sets what explosive the container uses
-     * @param ex - registered explosive handler
+     *
+     * @param ex   - registered explosive handler
      * @param size - size, or load of the explosive
-     * @param nbt - data used to trigger the explosive
+     * @param nbt  - data used to trigger the explosive
      * @return true if it was set, false if it was rejected
      */
     boolean setExplosive(IExplosiveHandler ex, double size, NBTTagCompound nbt);
+
+    /**
+     * Returns the NBTTag for additonal data used
+     * when spawning explosive blasts.
+     *
+     * @return null if no data
+     */
+    NBTTagCompound getAdditionalExplosiveData();
+
+    /**
+     * Gets the current size of the explosive
+     *
+     * @return size greater than 0
+     */
+    double getExplosiveSize();
 }
