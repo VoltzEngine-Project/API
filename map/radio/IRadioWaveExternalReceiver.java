b6e1e5dee7823959a5c3b06c894d2abdfee7b2e2
Robert Seifert <rseifert.phone@gmail.com>
2016-04-20 05:43:23 -0400

    Forgot to sync Radio Interfaces

---
+++ map/radio/IRadioWaveExternalReceiver.java
@@ -0,0 +1,22 @@
+package com.builtbroken.mc.api.map.radio;
+
+/**
+ * Applied to tiles that connect to {@link IRadioWaveReceiver} but are not antenna's
+ *
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 4/20/2016.
+ */
+public interface IRadioWaveExternalReceiver
+{
+    /**
+     * Called when a message is received
+     *
+     * @param hz            - frequency the message is on, ignore if this
+     *                      receive is tuned to a set frequency.
+     * @param sender        - object that sent the message.
+     * @param receiver      - antenna who picked up the message
+     * @param messageHeader - quick way to ID the data being passed into the message
+     * @param data          - data from the message
+     */
+    void receiveExternalRadioWave(float hz, IRadioWaveSender sender, IRadioWaveReceiver receiver, String messageHeader, Object[] data);
+}
---
+++ map/radio/IRadioWaveReceiver.java
@@ -0,0 +1,40 @@
+package com.builtbroken.mc.api.map.radio;
+
+import com.builtbroken.mc.api.IWorldPosition;
+import com.builtbroken.mc.lib.transform.region.Cube;
+
+/**
+ * Applied to objects that act like radio receivers
+ *
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 4/20/2016.
+ */
+public interface IRadioWaveReceiver extends IWorldPosition
+{
+    /**
+     * Called when a message is received
+     *
+     * @param hz            - frequency the message is on, ignore if this
+     *                      receive is tuned to a set frequency.
+     * @param sender        - object that sent the message.
+     * @param messageHeader - quick way to ID the data being passed into the message
+     * @param data          - data from the message
+     */
+    void receiveRadioWave(float hz, IRadioWaveSender sender, String messageHeader, Object[] data);
+
+    /**
+     * Gets the range of the receive as
+     * a cube.
+     *
+     * Keep in mind a receiver is
+     * a passive object and has a constant
+     * range. Use the distance
+     * from the sender to gauge strength
+     * of signal. Then boost the strength
+     * using logic rather than increasing
+     * range using power boosting.
+     *
+     * @return cube
+     */
+    Cube getRadioReceiverRange();
+}
