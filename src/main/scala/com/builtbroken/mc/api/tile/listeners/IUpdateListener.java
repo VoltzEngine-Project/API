e04307189f4df75a560f7f91e348ad63dda9d111
Robert Seifert <rseifert.phone@gmail.com>
2017-04-19 21:31:52 -0400

    Added more listener events

--- src/main/scala/com/builtbroken/mc/api/tile/listeners/IUpdateListener.java
+++ src/main/scala/com/builtbroken/mc/api/tile/listeners/IUpdateListener.java
@@ -1,5 +1,9 @@
 package com.builtbroken.mc.api.tile.listeners;
 
+import net.minecraft.world.World;
+
+import java.util.Random;
+
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 4/17/2017.
@@ -11,8 +15,46 @@ public interface IUpdateListener extends ITileEventListener
         return "update";
     }
 
+    /**
+     * Update trigger from a tile
+     *
+     * @param ticks
+     */
     default void update(long ticks)
     {
 
+    }
+
+    /**
+     * Update trigger from a block
+     *
+     * @param par5Random
+     */
+    default void updateTick(Random par5Random)
+    {
+
+    }
+
+    /**
+     * Update trigger from a block for graphics
+     *
+     * @param par5Random
+     */
+    default void randomDisplayTick(Random par5Random)
+    {
+
+    }
+
+    /**
+     * Called to get the tick rate
+     * <p>
+     * does not support location injection
+     * but does pass in world
+     *
+     * @return
+     */
+    default int tickRate(World world)
+    {
+        return -1;
     }
 }
