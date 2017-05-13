3deb158237076a160d4ae645a52ff575a1e8e2f2
Robert Seifert <rseifert.phone@gmail.com>
2017-05-13 03:09:28 -0400

    Documented IItemActivationListener

--- src/main/scala/com/builtbroken/mc/api/items/listeners/IItemActivationListener.java
+++ src/main/scala/com/builtbroken/mc/api/items/listeners/IItemActivationListener.java
@@ -5,11 +5,26 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 /**
+ * Handles any activation actions for an item. This includes left and right click of the item that would interact with the world.
+ * <p>
+ * Events are listened to with key "activation" by items.
+ *
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 5/9/2017.
  */
 public interface IItemActivationListener extends IItemEventListener
 {
+    /**
+     * Called when the player right clicks the item.
+     * <p>
+     * This is called for each listener so the passed in stack may not be the original stack. Order
+     * of listeners being called is not maintained so do not assume anything.
+     *
+     * @param stack  - item right clicked
+     * @param world  - world the item is in
+     * @param player - player holding the item
+     * @return original or modified stack
+     */
     default ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
     {
         return stack;
